d1db3078dbc45708b67162565e5ff0143808aa3c
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2024-03-01 19:26:35 -0700

    made some changes with practice

--- hangman_game/src/main.rs
+++ hangman_game/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
     println!("This is my Hangman game!");
+    
 }
+
+// write function that puts the underscores for length of word
+// word class that has all the info about a word and displays the word
+// user guesses
