c28ddd00bacf0190ddbc5ec45891d46afcea1513
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2024-03-19 20:37:45 -0600

    Added generating a random word and displaying rules of the game.

--- hangman_game/src/main.rs
+++ hangman_game/src/main.rs
@@ -4,8 +4,6 @@ use rand::Rng;
 use std::env;
 use std::fs;
 use std::collections::HashSet;
-// use std::fs::File;
-// use std::io::prelude;
 
 use std::{
     fs::File,
@@ -16,29 +14,33 @@ use std::{
 fn main() {
     println!("This is my Hangman game!");
 
-    // get_word();
     let mut word = String::new();
-    word = "face".to_string(); 
+    word = get_word();
 
-    // let mut wordVec: Vec<char> = Vec::new();   
-    // for letter in word.chars() {
-    //     wordVec.push('_');
-    // }
     let mut wordVec: Vec<char> = vec!['_'; word.len()];
     let mut correct_guesses: HashSet<char> = HashSet::new();
-    let mut guesses_remaining = 5;
+    let mut guesses_remaining = 8;
+
+    println!("Rules: ");
+    println!("1. You have 6 guesses to guess the word");
+    println!("2. Duplicate letters don't count as a guess");
+    println!("3. Guess letters until you lose or guess the word");
+    println!("4. Have fun!");
+    println!("Now, let's go!");
+    println!();
     
     loop {
         println!("Guess a letter: ({} guesses remaining)", guesses_remaining);
+
+        for letter in &wordVec {
+            print!("{} ", letter);
+        }
+        println!();
 
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        // let guess: char = match guess.trim().parse() {
-        //     Ok(guess) => guess,
-        //     Err(_) => continue,
-        // };
         let guess: char = match guess.trim().chars().next() {
             Some(c) => c,
             None => continue,
@@ -77,92 +79,78 @@ fn main() {
             println!("Out of guesses. The word was: {}", word);
             break;
         }
-
-        // TO DO
-        // create a list of letters that we can compare to so you don't reset the letters every time
-        // add the bad letters to print out
-        // give them only 5 guess or so
-        // make a win statement
-
-        
-        // display_word(&word, guess, wordVec.clone());
     }
 }
 
-fn display_word(word: &String, guess: char, mut wordVec: Vec<char>) {
-
-    // for letter in word.chars() {
-    //     // println!("This {letter}");
-    //     if letter == guess {
-    //         letters.push(letter);
-    //         // print!("{letter}");
-    //     } else {
-    //         if letter.is_alphabetic() {
-    //             letters.push(letter);
-    //         } else {
-    //             letters.push('_');
-    //             // print!("_")
-    //         }
-    //     }
-    //     // match letter {
-    //     //     {guess} => print!("{letter}"),
-    //     //     _ => print!("_"),
-    //     // };
-    //     // println!("_");
-    // }
-    // println!();
-    // println!("The word is {word}");
-    
-    
-    // match word.find(guess) {
-        //     Some(index) => println!("Found {} at index {}", guess, index),
-        //     None => println!("Character '{}' not found", guess),
-        // }
-        
-    match word.find(guess) {
-        Some(index) => {
-            println!("'{}' found. Good job!", guess);
-            
-            if let Some(letter) = wordVec.get_mut(index) {
-                *letter = guess;
-            }
-        },
-        None => println!("'{}' not found", guess),
-    }
-    
-    for letter in wordVec {
-        // println!("Printing out the word");
-        print!("{letter}");
-    }
-        
-}
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
     buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
+    .map(|l| l.expect("Could not parse line"))
+    .collect()    
 }
 
-fn populateList() {
-
-}
-
-fn get_word() {
+fn get_word() -> String {
     let rand_num = rand::thread_rng().gen_range(1..=828);
     // println!("Random number is: {rand_num}");
     let words = lines_from_file("words.txt");
-
+    
     if let Some(word) = words.get(rand_num - 1) {
         // println!("Random word: {}", word);
-        // display_word(word);
+        let mut random_word = String::new();
+        random_word = word.to_string();
+        return random_word;
     } else {
         println!("Failed to get word at index {}", rand_num);
+        return "face".to_string();
     }
 }
 
+// fn display_word(word: &String, guess: char, mut wordVec: Vec<char>) {
 
-// write function that puts the underscores for length of word
-// word class that has all the info about a word and displays the word
-// user guesses
+//     // for letter in word.chars() {
+//     //     // println!("This {letter}");
+//     //     if letter == guess {
+//     //         letters.push(letter);
+//     //         // print!("{letter}");
+//     //     } else {
+//     //         if letter.is_alphabetic() {
+//     //             letters.push(letter);
+//     //         } else {
+//     //             letters.push('_');
+//     //             // print!("_")
+//     //         }
+//     //     }
+//     //     // match letter {
+//     //     //     {guess} => print!("{letter}"),
+//     //     //     _ => print!("_"),
+//     //     // };
+//     //     // println!("_");
+//     // }
+//     // println!();
+//     // println!("The word is {word}");
+    
+    
+//     // match word.find(guess) {
+//         //     Some(index) => println!("Found {} at index {}", guess, index),
+//         //     None => println!("Character '{}' not found", guess),
+//         // }
+        
+//     match word.find(guess) {
+//         Some(index) => {
+//             println!("'{}' found. Good job!", guess);
+            
+//             if let Some(letter) = wordVec.get_mut(index) {
+//                 *letter = guess;
+//             }
+//         },
+//         None => println!("'{}' not found", guess),
+//     }
+    
+//     for letter in wordVec {
+//         // println!("Printing out the word");
+//         print!("{letter}");
+//     }
+        
+// }
