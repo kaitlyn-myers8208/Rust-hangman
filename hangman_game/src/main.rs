30eba30db0cd89e1930d3083bd3df21c98d196f4
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2024-03-19 20:21:45 -0600

    Fixed basic functioning of game

--- hangman_game/src/main.rs
+++ hangman_game/src/main.rs
@@ -3,6 +3,7 @@ use std::cmp::Ordering;
 use rand::Rng;
 use std::env;
 use std::fs;
+use std::collections::HashSet;
 // use std::fs::File;
 // use std::io::prelude;
 
@@ -18,22 +19,64 @@ fn main() {
     // get_word();
     let mut word = String::new();
     word = "face".to_string(); 
-    let mut letters: Vec<char> = Vec::new();   
-    for letter in word.chars() {
-        letters.push('_');
-    }
+
+    // let mut wordVec: Vec<char> = Vec::new();   
+    // for letter in word.chars() {
+    //     wordVec.push('_');
+    // }
+    let mut wordVec: Vec<char> = vec!['_'; word.len()];
+    let mut correct_guesses: HashSet<char> = HashSet::new();
+    let mut guesses_remaining = 5;
     
     loop {
-        println!("Guess a letter: ");
+        println!("Guess a letter: ({} guesses remaining)", guesses_remaining);
 
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
-            .expect("failed to read line");
-        let guess: char = match guess.trim().parse() {
-            Ok(guess) => guess,
-            Err(_) => continue,
-        };    
+            .expect("Failed to read line");
+        // let guess: char = match guess.trim().parse() {
+        //     Ok(guess) => guess,
+        //     Err(_) => continue,
+        // };
+        let guess: char = match guess.trim().chars().next() {
+            Some(c) => c,
+            None => continue,
+        };  
+
+        if correct_guesses.contains(&guess) {
+            println!("You already guessed '{}'", guess);
+            continue;
+        }
+
+        if word.contains(guess) {
+            println!("'{}' found. Good job!", guess);
+            correct_guesses.insert(guess);
+
+            for (index, letter) in word.chars().enumerate() {
+                if letter == guess {
+                    wordVec[index] = guess;
+                }
+            }
+        } else {
+            println!("'{}' not found", guess);
+            guesses_remaining -= 1;
+        }
+
+        if wordVec.iter().all(|&c| c != '_') {
+            println!("Congratulations! You guessed the word: {}", word);
+            break;
+        }
+
+        for letter in &wordVec {
+            print!("{} ", letter);
+        }
+        println!();
+
+        if guesses_remaining == 0 {
+            println!("Out of guesses. The word was: {}", word);
+            break;
+        }
 
         // TO DO
         // create a list of letters that we can compare to so you don't reset the letters every time
@@ -41,45 +84,57 @@ fn main() {
         // give them only 5 guess or so
         // make a win statement
 
-
-
-
-        // let guess: char = guess.trim().parse().expect("Please type a letter!");
-        // let mut file = File::open("words.txt");
         
-        display_word(&word, guess, letters.clone());
+        // display_word(&word, guess, wordVec.clone());
     }
 }
 
-fn display_word(word: &String, guess: char, mut letters: Vec<char>) {
-    // let length = word.len();
-    // let letters = 
-    // let user_guess = guess;
-    for letter in word.chars() {
-        // println!("This {letter}");
-        if letter == guess {
-            letters.push(letter);
-            // print!("{letter}");
-        } else {
-            if letter.is_alphabetic() {
-                letters.push(letter);
-            } else {
-                letters.push('_');
-                // print!("_")
+fn display_word(word: &String, guess: char, mut wordVec: Vec<char>) {
+
+    // for letter in word.chars() {
+    //     // println!("This {letter}");
+    //     if letter == guess {
+    //         letters.push(letter);
+    //         // print!("{letter}");
+    //     } else {
+    //         if letter.is_alphabetic() {
+    //             letters.push(letter);
+    //         } else {
+    //             letters.push('_');
+    //             // print!("_")
+    //         }
+    //     }
+    //     // match letter {
+    //     //     {guess} => print!("{letter}"),
+    //     //     _ => print!("_"),
+    //     // };
+    //     // println!("_");
+    // }
+    // println!();
+    // println!("The word is {word}");
+    
+    
+    // match word.find(guess) {
+        //     Some(index) => println!("Found {} at index {}", guess, index),
+        //     None => println!("Character '{}' not found", guess),
+        // }
+        
+    match word.find(guess) {
+        Some(index) => {
+            println!("'{}' found. Good job!", guess);
+            
+            if let Some(letter) = wordVec.get_mut(index) {
+                *letter = guess;
             }
-        }
-        // match letter {
-        //     {guess} => print!("{letter}"),
-        //     _ => print!("_"),
-        // };
-        // println!("_");
+        },
+        None => println!("'{}' not found", guess),
     }
-    for letter in letters {
+    
+    for letter in wordVec {
         // println!("Printing out the word");
         print!("{letter}");
     }
-    println!();
-    println!("The word is {word}");
+        
 }
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
@@ -94,18 +149,18 @@ fn populateList() {
 
 }
 
-// fn get_word() {
-//     let rand_num = rand::thread_rng().gen_range(1..=828);
-//     // println!("Random number is: {rand_num}");
-//     let words = lines_from_file("words.txt");
+fn get_word() {
+    let rand_num = rand::thread_rng().gen_range(1..=828);
+    // println!("Random number is: {rand_num}");
+    let words = lines_from_file("words.txt");
 
-//     if let Some(word) = words.get(rand_num - 1) {
-//         // println!("Random word: {}", word);
-//         display_word(word);
-//     } else {
-//         println!("Failed to get word at index {}", rand_num);
-//     }
-// }
+    if let Some(word) = words.get(rand_num - 1) {
+        // println!("Random word: {}", word);
+        // display_word(word);
+    } else {
+        println!("Failed to get word at index {}", rand_num);
+    }
+}
 
 
 // write function that puts the underscores for length of word
